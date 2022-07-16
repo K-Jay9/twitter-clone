4225327643447bcc27766b38927ea426cf6e59bd
kjempire9 <kjempirecode@gmail.com>
2022-07-16 17:06:08 +0300

    The dummy data for the customers class

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO CUSTOMER
+(USERNAME, EMAIL, LOCATION, NAME, FOLLOWER_COUNT, FOLLOWERS, FOLLOWING, FOLLOWING_COUNT)
+VALUES
+('karomo', 'karomo@gmail.com', 'Nairobi', 'Joe karomo',25,'famous','kidding',23);
+
+INSERT INTO CUSTOMER
+(USERNAME, EMAIL, LOCATION, NAME, FOLLOWER_COUNT, FOLLOWERS, FOLLOWING, FOLLOWING_COUNT)
+VALUES
+('onyango', 'onyango@gmail.com', 'Kisumu', 'Felix karomo',25,'famous','kidding',23);
+
+INSERT INTO CUSTOMER
+(USERNAME, EMAIL, LOCATION, NAME, FOLLOWER_COUNT, FOLLOWERS, FOLLOWING, FOLLOWING_COUNT)
+VALUES
+('Mutiso', 'Mutiso@gmail.com', 'Machakos', 'Brian Mutiso',25,'famous','kidding',23);
+
+
+INSERT INTO CUSTOMER
+(USERNAME, EMAIL, LOCATION, NAME, FOLLOWER_COUNT, FOLLOWERS, FOLLOWING, FOLLOWING_COUNT)
+VALUES
+('Kamea', 'Kamea@gmail.com', 'Nairobi', 'Henry Kamea',25,'famous','kidding',23);
+
+
+INSERT INTO CUSTOMER
+(USERNAME, EMAIL, LOCATION, NAME, FOLLOWER_COUNT, FOLLOWERS, FOLLOWING, FOLLOWING_COUNT)
+VALUES
+('Shosho', 'Shosho@gmail.com', 'Kinangop', 'Mimi Shosho',25,'famous','kidding',23);
+
+
+
